025941473c677b818a6c27c386d3896046793bdc
Tiago Souza <tiagoice@hotmail.com>
2016-08-28 21:15:04 -0300

    inicio da implementação

--- src/controller/ControllerCadastro.java
+++ src/controller/ControllerCadastro.java
@@ -1,8 +1,67 @@
 package controller;
 
-public class ControllerCadastro {
+import org.quartz.CronScheduleBuilder;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SchedulerFactory;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
 
-	void iniciarLeitura(){
-		
+import job.MyJob;
+
+public class ControllerCadastro {
+	private SchedulerFactory schedFact;
+	private JobDetail job;
+	public static ControllerCadastro instance;
+	
+	public static ControllerCadastro getInstance(){
+		if(instance==null){
+			instance=new ControllerCadastro();
+		}
+		return instance;
 	}
+	public void iniciarProcesso(){
+		//CadastroDao cd = new CadastroDao();
+		//List<CadastroBean>listaCadastro= cd.lerCadastro();
+        try {
+            schedFact = new StdSchedulerFactory();
+            Scheduler sched = schedFact.getScheduler();
+            sched.start();
+            
+            job = JobBuilder.newJob(MyJob.class)
+                .withIdentity("myjob", "grupo1")
+                .build();
+            
+            Trigger trigger = TriggerBuilder
+                .newTrigger()
+                .withIdentity("meugatilho", "grupo1")
+                .withSchedule(CronScheduleBuilder.cronSchedule("0/10 * * * * ?"))
+//                .withSchedule(CronScheduleBuilder.cronSchedule("0 01 11 ? * MON,TUE,WED,THU,FRI,SAT"))
+                .build();
+            sched.scheduleJob(job, trigger);
+             
+            sched.deleteJob(job.getKey());
+            trigger = TriggerBuilder.newTrigger()
+            	.withIdentity("meugatilho", "grupo1")
+ 	            .withSchedule(CronScheduleBuilder.cronSchedule("0/5 * * * * ?"))
+ 	            .build();
+            sched.scheduleJob(job,trigger);
+            
+        } catch (SchedulerException e) {
+            System.out.println("erro");
+            e.printStackTrace();
+        }
+	}
+	public void pararProcesso(){
+		//
+	}
+	public void pegarAgendamentoBD(){
+		//Agenda agenda = new Agenda();
+		//iniciarProcesso(agenda);
+	}
+	
+
 }
---
+++ src/job/MyJob.java
@@ -0,0 +1,13 @@
+package job;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class MyJob implements Job {
+
+	@Override
+	public void execute(JobExecutionContext arg0) throws JobExecutionException {
+	}
+
+}
