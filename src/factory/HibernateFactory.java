18bf0a110d0d4c71dc58c7be744e9bbf34b38a50
Tiago Souza <tiagoice@hotmail.com>
2016-09-22 17:46:11 -0300

    Alterações registro de log

--- src/factory/HibernateFactory.java
+++ src/factory/HibernateFactory.java
@@ -4,40 +4,48 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class HibernateFactory {
 	//recebendo a sessao
 	public Session getSession(){
-		SessionFactory fabrica = new Configuration()
-        		.configure()
-        		.buildSessionFactory();
-		return fabrica.openSession();
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+		SessionFactory sessionFactory = null;
+		try{
+			sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+		}catch(Exception e){
+			StandardServiceRegistryBuilder.destroy(registry);
+			e.printStackTrace();
+		}
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		return session;
 	}
 	//returnando a lista de objetos da sessao
-	@SuppressWarnings({ "deprecation", "unchecked" })
+	@SuppressWarnings("unchecked")
 	public List<Object> getList(Session session, Object object){
-		return session.createQuery("from "+object).list();
+		List<Object> list = (List<Object>)session.createQuery("from "+object)
+				.getResultList();
+		return list;
 	}
 	//retornando apenas 1 objeto
-
 	public Object getObject(Session session, Object object){
 		return session.createQuery("from "+object).getFirstResult();
 	}
 	//salvar ou atualiza
 	public String  saveOrUpdateSession(Session session, Object object){
 		try{
-			session.beginTransaction();
 			session.saveOrUpdate(object);
-			session.getTransaction().commit();
-			return "Salvo com sucesso";
+			return "Salvo";
 		}catch(Exception e){
-			return "Erro ao Salvar";
+			return "Erro ao Salvar: "+e.getMessage();
 		}
 	}
-	
 	//fechando a sessao
 	public void closeSession(Session session){
+		session.getTransaction().commit();
 		session.close();
 	}
 
